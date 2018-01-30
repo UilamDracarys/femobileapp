c2fd214991e7d6db69cfc4202eebef9b3222590d
William <williamcernestojr@yahoo.com>
2018-01-30 08:58:32 +0800

    Added Multi-Choice Spinner.

--- main/java/com/scbpfsdgis/femobilebetav20/FieldDetailActivity.java
+++ main/java/com/scbpfsdgis/femobilebetav20/FieldDetailActivity.java
@@ -4,34 +4,49 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-import com.scbpfsdgis.femobilebetav20.data.model.Fields;
+import com.guna.libmultispinner.MultiSelectionSpinner;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 
 /**
  * Created by William on 1/27/2018.
  */
 
-public class FieldDetailActivity extends AppCompatActivity {
+public class FieldDetailActivity extends AppCompatActivity implements MultiSelectionSpinner.OnMultipleItemsSelectedListener {
 
-    Spinner spnSuit;
+    Spinner spnMechMeth, spnTract;
+    String limits = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_field_detail);
 
-        spnSuit = findViewById(R.id.spnSuit);
-        String[] suit = getResources().getStringArray(R.array.suitability);
+        String[] limits = getResources().getStringArray(R.array.limitations);
+        List<String> limitList = new ArrayList<String>(Arrays.asList(limits));
 
-        List<String> suitList = new ArrayList<String>(Arrays.asList(suit));
-        suitList.add(0, "-Choose Suitability-");
+        MultiSelectionSpinner multiSelectionSpinner = (MultiSelectionSpinner) findViewById(R.id.spnMainLim);
+        multiSelectionSpinner.setItems(limitList);
+        multiSelectionSpinner.setListener(this);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, suitList);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-        spnSuit.setAdapter(adapter);
+        spnMechMeth = findViewById(R.id.spnMechMeth);
+        spnTract = findViewById(R.id.spnTract);
+
+        System.out.println("Selected Strings: " + multiSelectionSpinner.getSelectedItemsAsString());
+
+    }
+
+    @Override
+    public void selectedIndices(List<Integer> indices) {
+
+    }
+
+    @Override
+    public void selectedStrings(List<String> strings) {
+        Toast.makeText(this, strings.toString(), Toast.LENGTH_LONG).show();
+        System.out.println("Toast " + strings.toString());
     }
 }
